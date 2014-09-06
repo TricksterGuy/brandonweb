93609ced96d3edf9fd7c7e580df74edc7d46f1ce
Brandon <tricksterguy@live.com>
2014-09-05 18:15:59 -0700

    initial

---
+++ app/helpers/brandon_web_helper.rb
@@ -0,0 +1,2 @@
+module BrandonWebHelper
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'brandon_web/index'
+  get 'brandon_web/mode0'
+  get 'brandon_web/mode3'
+  get 'brandon_web/mode4'
+  get 'brandon_web/sprites'
+  post 'brandon_web/generate'
+end

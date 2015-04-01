e63c98640aff364443bc34ecace87c7d323fd576
Brandon <tricksterguy@live.com>
2015-03-31 22:39:13 -0700

    Remove everything :D

--- README.md
+++
@@ -1,4 +0,0 @@
-brandonweb
-==========
-
-Rails app
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get 'brandon_web/index'
-  get 'brandon_web/mode0'
-  get 'brandon_web/mode3'
-  get 'brandon_web/mode4'
-  get 'brandon_web/sprites'
-  post 'brandon_web/generate'
-end
